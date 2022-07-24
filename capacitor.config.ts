3573c1a78c5594db88e5da3bfb8867ea8677593d
Kamlesh Kumar <kaundal.k.k@gmail.com>
2022-07-24 23:13:21 +0530

    Kamlesh

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.slinfy.crm',
+  appName: 'slinfy-crm-angular',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
